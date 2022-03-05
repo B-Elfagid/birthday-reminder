cd67088dc7898f28cbb8d8b912ed768bd24261ad
Betty <betty_elfagid@yahoo.co.uk>
2022-03-05 13:55:27 +0000

    data.js file and array of data added

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export default [
+    {
+       id: 1,
+       name: 'Theadora Giaquinto',
+       age: 1,
+       image: 'https://cdn.pixabay.com/photo/2016/05/12/23/03/lamb-1388937_960_720.png',
+     
+    },
+
+    {
+        id: 1,
+        name: 'Davide Giaquinto',
+        age: 33,
+        image: 'https://cdn.pixabay.com/photo/2018/03/31/06/31/dog-3277416__340.jpg',
+      
+     },
+
+     {
+        id: 1,
+        name: 'Yanet Dereje',
+        age: 10,
+        image: 'https://hs.sbcounty.gov/cn/Photo%20Gallery/Sample%20Picture%20-%20Koala.jpg',
+      
+     },
+
+     {
+        id: 1,
+        name: 'Naod Dereje',
+        age: 12,
+        image: 'https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782__340.jpg',
+      
+     },
+
+     {
+        id: 1,
+        name: 'Dagim Dereje',
+        age: 8,
+        image: 'https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045__340.jpg',
+      
+     },
+
+     {
+        id: 1,
+        name: 'Melat Birhanu',
+        age: 28,
+        image: 'https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074__340.jpg',
+      
+     },
+
+     {
+        id: 1,
+        name: 'Dereje Fantaye',
+        age: 45,
+        image: ddd,
+      
+     },
+
+     {
+        id: 1,
+        name: 'Marta Birhanu',
+        age: 40,
+        image: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554__340.jpg',
+      
+     }
+
+]
