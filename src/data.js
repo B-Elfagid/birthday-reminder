35e1f04a602ed4db19f88ea1411fd64886c91185
Betty <betty_elfagid@yahoo.co.uk>
2022-03-05 14:15:29 +0000

    add list.js and and define the app.js function, import the component and render

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export default [
     },
 
     {
-        id: 1,
+        id: 2,
         name: 'Davide Giaquinto',
         age: 33,
         image: 'https://cdn.pixabay.com/photo/2018/03/31/06/31/dog-3277416__340.jpg',
@@ -16,7 +16,7 @@ export default [
      },
 
      {
-        id: 1,
+        id: 3,
         name: 'Yanet Dereje',
         age: 10,
         image: 'https://hs.sbcounty.gov/cn/Photo%20Gallery/Sample%20Picture%20-%20Koala.jpg',
@@ -24,7 +24,7 @@ export default [
      },
 
      {
-        id: 1,
+        id: 4,
         name: 'Naod Dereje',
         age: 12,
         image: 'https://cdn.pixabay.com/photo/2015/11/16/14/43/cat-1045782__340.jpg',
@@ -32,7 +32,7 @@ export default [
      },
 
      {
-        id: 1,
+        id: 5,
         name: 'Dagim Dereje',
         age: 8,
         image: 'https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045__340.jpg',
@@ -40,7 +40,7 @@ export default [
      },
 
      {
-        id: 1,
+        id: 6,
         name: 'Melat Birhanu',
         age: 28,
         image: 'https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074__340.jpg',
@@ -48,15 +48,15 @@ export default [
      },
 
      {
-        id: 1,
+        id: 7,
         name: 'Dereje Fantaye',
         age: 45,
-        image: ddd,
+        image: 'https://images.pexels.com/photos/64152/tiger-cub-tiger-cub-big-cat-64152.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       
      },
 
      {
-        id: 1,
+        id: 8,
         name: 'Marta Birhanu',
         age: 40,
         image: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554__340.jpg',
