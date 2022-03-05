0b87dcd247ff3b1b0314e6d861e89fab4a9666a7
Betty <betty_elfagid@yahoo.co.uk>
2022-03-05 15:31:05 +0000

    list component and functionality added and iterate through people and render the people list

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const [people, setPeople] = useState(data)
 
   return <main>
       <section className='container'>
-         <h3>0 birthdays today</h3>
+         <h3>{people.length} birthdays today</h3>
          <List people={people}/>
          <button onClick={()=>console.log('youclickedme')}>Clear All</button>
       </section>
